721d2c1a16c9609e5e44f21fc84d1a25625ce50f
Matt Lewis <matthewlewis@improbable.io>
2022-12-01 23:38:42 +0000

    day 1 and initial rough framework

---
+++ src/days/day_1.rs
@@ -0,0 +1,61 @@
+fn to_u32(i: &str) -> u32 {
+    i.trim().parse::<u32>().unwrap()
+}
+
+fn sum_of_snacks(snacks: &str) -> u32 {
+    snacks.lines().map(to_u32).sum()
+}
+
+pub fn part_a(input: &str) -> u32 {
+    let snacks_per_elf = input.split("\n\n").collect::<Vec<&str>>();
+    let calories_per_elf = snacks_per_elf.iter().map(|x| {sum_of_snacks(*x)}).collect::<Vec<u32>>();
+    *calories_per_elf.iter().max().unwrap()
+}
+
+pub fn part_b(input: &str) -> u32 {
+    let snacks_per_elf = input.split("\n\n").collect::<Vec<&str>>();
+    let mut calories_per_elf = snacks_per_elf.iter().map(|x| {sum_of_snacks(*x)}).collect::<Vec<u32>>();
+    calories_per_elf.sort();
+    calories_per_elf.iter().rev().take(3).sum::<u32>()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ptr::read;
+
+    use advent_of_code_2022::read_test_file;
+
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_to_u32() {
+        assert_eq!(to_u32("123"), 123);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_to_u32_bad_input() {
+        to_u32("not_a_u32");
+    }
+
+    #[test]
+    fn test_sum_of_snacks() {
+        let snacks = "1
+            2
+            100";
+        assert_eq!(sum_of_snacks(snacks), 103);
+    }
+
+    #[test]
+    fn test_part_a() {
+        let input = read_test_file(1, 'a');
+        assert_eq!(part_a(&input), 101);
+    }
+
+    #[test]
+    fn test_part_b() {
+        let input = read_test_file(1, 'b');
+        assert_eq!(part_b(&input), 106);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,40 @@
+extern crate argparse;
+use advent_of_code_2022::read_file;
+use days::*;
+use argparse::{ArgumentParser, Store};
+mod days;
+
+
+fn main() {
+    let mut day = 0;
+    let mut part = ' ';
+    {
+        // this block limits scope of borrows by ap.refer() method
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Run Advent of Code 2022.");
+        ap.refer(&mut day)
+            .add_option(&["-d", "--day"], Store, "Day to run");
+        ap.refer(&mut part)
+            .add_option(&["-p", "--part"], Store, "Part to run [a, b]");
+        ap.parse_args_or_exit();
+    }
+
+    if day < 1 || day > 25 {
+        panic!("--day parameter must be specified and be between `1` and `25` inclusive");
+    }
+
+    let valid_parts = vec!['a', 'b'];
+    if !valid_parts.contains(&part) {
+        panic!("--part parameter must be specified and be set either `a` or `b`");
+    }
+
+    let input = read_file(day, part);
+
+    let result = match (day, part) {
+        (1, 'a') => day_1::part_a(&input),
+        (1, 'b') => day_1::part_b(&input),
+        (_, _) => panic!("Unrecognised day [{}] part [{}]", day, part),
+    };
+
+    println!("Result: {}", result)
+}
